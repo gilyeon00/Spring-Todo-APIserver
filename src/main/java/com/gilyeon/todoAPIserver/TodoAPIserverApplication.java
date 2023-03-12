b1c23bf17caf1d0de899ba687b5678e24bf9de62
gilyeon00 <12venus08@naver.com>
2023-03-12 22:02:32 +0900

    main method 작성

--- src/main/java/com/gilyeon/todoAPIserver/TodoAPIserverApplication.java
+++ src/main/java/com/gilyeon/todoAPIserver/TodoAPIserverApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoAPIserverApplication {
 
 	public static void main(String[] args) {
-		System.out.println("HEllo");
+		SpringApplication.run(TodoAPIserverApplication.class, args);
 	}
 
 }
